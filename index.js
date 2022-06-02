a76ef0a53b7f1944ae4189a514bae0c81ebbb16f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-06-02 16:57:17 +0530

    commit

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
+app.get("/", (req, res) => {
+  
+  res.send("Welcome Bloggers");
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
